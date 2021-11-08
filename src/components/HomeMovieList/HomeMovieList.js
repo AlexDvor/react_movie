4b897a1b6053aa295ce08e8f4a221b4f88d675ae
Oleksii <alexmatvichuk@gmail.com>
2021-11-08 14:01:21 +0100

    add slider

--- src/components/HomeMovieList/HomeMovieList.js
+++ src/components/HomeMovieList/HomeMovieList.js
@@ -1,28 +1,69 @@
 import { Link } from 'react-router-dom';
 import { MovieList, MovieItem, WrapperThumb, WrapperContent, Image } from './HomeMovieList.styled';
+import Slider from 'react-slick';
 
 export default function HomeMovieList({ movies }) {
   const URL = 'https://image.tmdb.org/t/p/w500/';
+  console.log(movies);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 5,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+
+      {
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+    ],
+  };
+
   return (
-    <MovieList>
+    <Slider {...settings}>
       {movies.length > 0 &&
         movies.map(({ id, poster_path, title }) => (
-          <MovieItem key={id}>
-            <Link
-              to={{
-                pathname: `/movies/${id}`,
-              }}
-            >
-              <WrapperThumb>
-                <Image src={`${URL}/${poster_path}`} alt={title} width="200px" />
-              </WrapperThumb>
-              <WrapperContent>
-                <h2>{title}</h2>
-              </WrapperContent>
-            </Link>
-          </MovieItem>
+          <WrapperThumb>
+            <Image src={`${URL}/${poster_path}`} alt={title} width="200px" />
+          </WrapperThumb>
         ))}
-    </MovieList>
+    </Slider>
   );
 }
 
--- src/components/HomeMovieList/HomeMovieList.styled.jsx
+++ src/components/HomeMovieList/HomeMovieList.styled.jsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const MovieList = styled.ul`
   list-style: none;
@@ -15,9 +17,10 @@ export const MovieItem = styled.li`
 `;
 
 export const WrapperThumb = styled.div`
-  max-width: 200px;
+  max-width: 425px;
 `;
 export const WrapperContent = styled.div``;
+
 export const Image = styled.img`
   width: 100%;
   height: 100%;
