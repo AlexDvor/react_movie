8f3241179293c44f9b919c435b8d079f9f3b3953
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-02-12 20:17:14 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -7,28 +7,36 @@ import MyListPage from './pages/MyListPage/MyListPage';
 import SearchPage from './pages/SearchPage/SearchPage';
 import { Footer } from './components/Footer/Footer';
 
+import { Main, WrapperFooter } from './App.styled';
+
 function App() {
   return (
     <>
       <AppBar></AppBar>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
 
-        <Route exact path="/movies/:movieId">
-          <AboutMoviePage />
-        </Route>
+      <Main>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-        <Route exact path="/my_list">
-          <MyListPage />
-        </Route>
+          <Route exact path="/movies/:movieId">
+            <AboutMoviePage />
+          </Route>
 
-        <Route exact path="/search">
-          <SearchPage />
-        </Route>
-      </Switch>
-      <Footer />
+          <Route exact path="/my_list">
+            <MyListPage />
+          </Route>
+
+          <Route exact path="/search">
+            <SearchPage />
+          </Route>
+        </Switch>
+      </Main>
+
+      <WrapperFooter>
+        <Footer />
+      </WrapperFooter>
     </>
   );
 }
---
+++ src/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Main = styled.div`
+  min-height: calc(100vh - 20px);
+`;
+
+export const WrapperFooter = styled.div`
+  position: relative;
+  left: 0;
+  bottom: 0;
+`;
