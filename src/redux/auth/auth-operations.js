7b4330621a4fd9694adad1fd45c476aef3464023
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-02-21 19:20:18 +0100

    change baseURL

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:4040/api';
+// axios.defaults.baseURL = 'http://localhost:4040/api';
+axios.defaults.baseURL = 'https://watchentrailer.herokuapp.com/api';
 
 const token = {
   set(token) {
