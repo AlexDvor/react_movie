91dc07f8d23d7ff90530bd280429eb0a8d08db44
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-03-19 22:04:08 +0100

    add link to home for logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import { WrapperLogo, Img } from './Logo.styled';
 import imageLogo from '../../images/logo.png';
+import { Link } from 'react-router-dom';
 
 export default function Logo({ width, position }) {
   return (
     <>
-      <WrapperLogo width={width} position={position}>
-        <Img src={imageLogo} alt="logo" />
-      </WrapperLogo>
+      <Link to="/home">
+        <WrapperLogo width={width} position={position}>
+          <Img src={imageLogo} alt="logo" />
+        </WrapperLogo>
+      </Link>
     </>
   );
 }
